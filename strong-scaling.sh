ae2d089b914ae792b0cfb6d18afecd1310ee2f7a
PMV05 <pedro.vazquez13@upr.edu>
2025-05-13 09:17:21 -0400

    File completos

--- strong-scaling.sh
+++ strong-scaling.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -J mv_strong
-#SBATCH -A gen243
-#SBATCH -N 8
+#SBATCH -A trn035
+#SBATCH -N 8  
 #SBATCH --ntasks-per-node=32
-#SBATCH -t 00:10:00
-#SBATCH -o logs/strong_%j.out
+#SBATCH -t 00:15:00
+#SBATCH -o strong_%j.out
 
 module purge
 module load PrgEnv-gnu
@@ -17,4 +17,5 @@ N=50000
 for P in 32 64 128 256; do
     nodes=$((P/32))
     echo "---- P=$P (NODES=$nodes) ----"
-    srun -N $nodes -n $P python mv_strong.py $N
+    srun -N $nodes -n $P python strscaling.py $N
+done
--- strscaling.py
+++ strscaling.py
@@ -1,36 +1,85 @@
 from mpi4py import MPI
 import numpy as np
-import time
 import sys
 
+# Incializa el MPI
 comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
+processRank = comm.Get_rank()    
+processCount = comm.Get_size()    
 
-N = int(sys.argv[1])
+# Recibe el tamaño de la matriz via el bash
+matrixSize = int(sys.argv[1])
 
-rows = N // size
-startRow = rank * rows
-endRow = (rank + 1) * rows if rank != size - 1 else N
+# Determina la cantidad de filas que cada proceso realizara
+rowsPerProcess = [
+    matrixSize // processCount + (1 if i < matrixSize % processCount else 0)
+    for i in range(processCount)
+]
+# Determina cual sera la posicion incial de cada proceso
+rowDisplacements = [sum(rowsPerProcess[:i]) for i in range(processCount)]
 
-A = np.random.randint(1, 100, size=(N, N))
-B = np.random.randint(1, 100, size=(N, N))
+# Asigna la cantidad de filas al proceso 
+localRowCount = rowsPerProcess[processRank]
 
-local_A = A[startRow:endRow, :]
-local_B = B[startRow:endRow, :]
+# Guarda el espacio en memoria
+localMatrix = np.empty((localRowCount, matrixSize), dtype=np.int32)
+vectorX = np.empty(matrixSize,              dtype=np.int32)
 
+# Inicializa la matriz entera y el vector, y divide la matriz en sectores que se enviaran a los demas procesos
+if processRank == 0:
+    fullMatrix = np.random.randint(1, 100, size=(matrixSize, matrixSize), dtype=np.int32)
+    vectorX[:] = np.random.randint(1, 100, size=matrixSize, dtype=np.int32)
+    for dest in range(1, processCount):
+        start = rowDisplacements[dest]
+        count = rowsPerProcess[dest]
+        comm.Send(
+            [fullMatrix[start:start+count, :], MPI.INT],
+            dest=dest,
+            tag=77
+        )
+        # Guarda el primer bloque de filas para el proceso 0
+    localMatrix[:] = fullMatrix[:rowsPerProcess[0], :]
+else:
+    # Reparte la matriz a los demas procesos 
+    comm.Recv(
+        [localMatrix, MPI.INT],
+        source=0,
+        tag=77
+    )
+
+# Transmite el vector a todos los procesos comenzando en el proceso 0
+comm.Bcast([vectorX, MPI.INT], root=0)
+
+# Espera a todos los procesos y comienza el timer
 comm.Barrier()
-startTime = time.time()
+startTime = MPI.Wtime()
 
-local_result = local_A * local_B
+# Realiza la multiplicacion del vector
+localResult = localMatrix.dot(vectorX)
 
+# Adjunta todos los resultados y los introduce al vector en el proceso 0
+processRowCounts = np.array(rowsPerProcess,    dtype='i')
+processRowDisplacements = np.array(rowDisplacements, dtype='i')
+if processRank == 0:
+    resultVector = np.empty(matrixSize, dtype=np.int32)
+else:
+    resultVector = None
+
+comm.Gatherv(
+    sendbuf=[localResult, MPI.INT],
+    recvbuf=[resultVector, (processRowCounts, processRowDisplacements), MPI.INT],
+    root=0
+)
+
+# Espera todos los procesos y termina el timer
 comm.Barrier()
-endTime = time.time()
+endTime = MPI.Wtime()
 
-finished = comm.gather(local_result, root=0)
+# Encuentra el tiempo total de todos los procesos
+localElapsed = endTime - startTime
+maxElapsed = comm.reduce(localElapsed, op=MPI.MAX, root=0)
 
-if rank == 0:
-    full_result = np.vstack(finished)
-    print(f"matriz {N}x{N} usando {size} procesos")
-    print(f"Time: {endTime - startTime:.6f} segundos")
-    print(full_result)
+# Proceso numero 0 imprime el tiempo total de ejecucion
+if processRank == 0:
+    print(f"Computed y = Matrix[{matrixSize}×{matrixSize}] × Vector[{matrixSize}] in {maxElapsed:.6f} seconds")
+
